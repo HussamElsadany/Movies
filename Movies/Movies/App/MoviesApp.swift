17fad76d495a0d28f245dfc8a93037c98c84613a
HussamElsadany <hussam.elsadany@gmail.com>
2023-01-27 03:26:17 +0200

    update app with presentation layer

--- Movies/Movies/App/MoviesApp.swift
+++ Movies/Movies/App/MoviesApp.swift
@@ -1,25 +1,19 @@
-//
-//  MoviesApp.swift
-//  Movies
-//
-//  Created by xdmgzdev on 12/04/2021.
-//
-
 import SwiftUI
+import Presentation
 
 @main
 struct MoviesApp: App {
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-        .onAppear(perform: setAppareance)
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onAppear(perform: setAppareance)
+        }
     }
-  }
 }
 
 private extension MoviesApp {
-  func setAppareance() {
-    let attrs = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30)]
-    UINavigationBar.appearance().largeTitleTextAttributes = attrs
-  }
+    func setAppareance() {
+        let attrs = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 30)]
+        UINavigationBar.appearance().largeTitleTextAttributes = attrs
+    }
 }
